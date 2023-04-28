1cd76d131d7cb9962082efc567b83c8ad6fc4b6d
Jiacheng-coder <2253541907@qq.com>
2023-04-29 00:17:41 +0800

    workouts-finished

--- src/components/WorkoutSlider.js
+++ src/components/WorkoutSlider.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {workouts} from '../data'
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import '../workoutSlider.css'
+
+import {Navigation} from 'swiper'
 
 const WorkoutSlider = () => {
-  return <div>WorkoutSlider</div>;
+  const {programs} = workouts
+  return <Swiper slidesPerView={2} spaceBetween={32} navigation={true} breakpoints={{
+    768:{
+      slidesPerView:3
+    },
+    1024:{
+      slidesPerView:4
+    },
+  }} modules={[Navigation]} className='workoutSlider'>
+    {programs.map((program,idx)=>{
+      const {image,name}=program
+      return <SwiperSlide key={idx} className='max-w-[320px] max-h-[320px] relative'>
+        <img src={image} alt="" className='w-full h-full object-cover'/>
+        <div className='absolute left-[20px] bottom-[20px] bg-white h-[26px] px-[14px] flex items-center rounded-[1px]'>
+          <div className='font-primary font-semibold text-sm text-neutral-500'>{name}</div>
+        </div>
+      </SwiperSlide>
+    })}
+  </Swiper>;
 };
 
 export default WorkoutSlider;
--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,7 +1,22 @@
 import React from 'react';
-
+import {workouts} from '../data'
+import WorkoutSlider from '../components/WorkoutSlider'
 const Workouts = () => {
-  return <div>Workouts</div>;
+  const {title,icon}=workouts
+  return <section className="section">
+    <div className='section-title-group max-w-[540px] mx-auto px-4 lg:px-0' data-aos='fade-up' data-aos-delay='200'>
+      <img src={icon} alt="" />
+      <h2 className='h2 section-title'>
+        {title}
+        <span className="text-primary-200">
+          .
+        </span>
+      </h2>
+    </div>
+    <div data-aos='fade-up' data-aos-delay='300'>
+      <WorkoutSlider/>
+    </div>
+  </section>
 };
 
 export default Workouts;
