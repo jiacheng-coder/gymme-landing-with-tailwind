5539aafa3cc7c737b29e3e20c6d62f535752f144
Jiacheng-coder <2253541907@qq.com>
2023-04-29 10:23:33 +0800

    pricing-planList-finished

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import {pricing} from '../data'
+import PlanList from './PlanList'
 const Pricing = () => {
-  return <div>Pricing</div>;
+  const {icon,title,plans}=pricing
+  return <section className="section">
+    <div className='section-title-group max-w-[540px] mx-auto px-4 lg:px-0' data-aos='fade-up' data-aos-offset='200' data-aos-delay='200'>
+      <img src={icon} alt="" />
+      <h2 className='h2'>{title}
+        <span className="text-primary-200">
+          .
+        </span>
+      </h2>
+    </div>
+    <PlanList plans={plans}/>
+  </section>
 };
 
 export default Pricing;
